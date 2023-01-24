23808472ca74042f3accf06bb459e9efba870eb7
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-01-24 15:19:14 -0600

    Add js file to html

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function stringLength(string) {
+  if (string.length === 0 || string.length > 10) {
+    throw new Error("too big or empty string");
+  }
   return string.length;
 }
 
-module.exports = stringLength;
+export default stringLength;
